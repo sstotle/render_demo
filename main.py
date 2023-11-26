13c3e1c689cbb4c9facbc69a3b26e1af423fe96a
senyp-test <135482766+senyp-test@users.noreply.github.com>
2023-11-27 00:33:57 +0100

    changes

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return redirect('https://www.google.com')
+    return redirect("https://google.com")
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
